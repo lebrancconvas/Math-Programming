9f6a3a4e282a558a8ff611d26d7fc57309b6d658
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-10-18 22:44:23 +0700

    Add Logic Method and Remove Test Case.

--- logic.js
+++ logic.js
@@ -12,7 +12,11 @@ let logic = (left,right,operation) => {
         else if (left === false && right === false) {
             return false
         }
+        else {
+            return "--ERROR--"
+        }
     }
+
     else if(operation === 'and') {
         if (left === true && right === true) {
             return true
@@ -26,7 +30,11 @@ let logic = (left,right,operation) => {
         else if (left === false && right === false) {
             return false
         }
+        else {
+            return "--ERROR--"
+        }
     }
+
     else if(operation === 'ifthen') {
         if (left === true && right === true) {
             return true
@@ -40,7 +48,11 @@ let logic = (left,right,operation) => {
         else if (left === false && right === false) {
             return true
         }
+        else {
+            return "--ERROR--"
+        }
     }
+
     else if(operation === 'onlyif') {
         if (left === true && right === true) {
             return true
@@ -54,7 +66,98 @@ let logic = (left,right,operation) => {
         else if (left === false && right === false) {
             return true
         }
+        else {
+            return "--ERROR--"
+        }
+    }
+
+    else if(operation === 'inverse') {
+        if(left === true && right === null) {
+            return false
+        }
+        else if(left === false && right === null) {
+            return true
+        }
+        else if(left === null && right === true) {
+            return false
+        }
+        else if(left === null && right === false) {
+            return true
+        }
+        else {
+            return "--ERROR--"
+        }
+    }
+
+    else if(operation === 'nor') {
+        if (left === true && right === true) {
+            return false
+        }
+        else if (left === true && right === false) {
+            return false
+        }
+        else if (left === false && right === true) {
+            return false
+        }
+        else if (left === false && right === false) {
+            return true
+        }
+        else {
+            return "--ERROR--"
+        }
+    }
+
+    else if(operation === 'nand') {
+        if (left === true && right === true) {
+            return false
+        }
+        else if (left === true && right === false) {
+            return true
+        }
+        else if (left === false && right === true) {
+            return true
+        }
+        else if (left === false && right === false) {
+            return true
+        }
+        else {
+            return "--ERROR--"
+        }
+    }
+
+    else if(operation === 'xor') {
+        if (left === true && right === true) {
+            return false
+        }
+        else if (left === true && right === false) {
+            return true
+        }
+        else if (left === false && right === true) {
+            return true
+        }
+        else if (left === false && right === false) {
+            return false
+        }
+        else {
+            return "--ERROR--"
+        }
+    }
+
+    else if(operation === 'xnor') {
+        if (left === true && right === true) {
+            return true
+        }
+        else if (left === true && right === false) {
+            return false
+        }
+        else if (left === false && right === true) {
+            return false
+        }
+        else if (left === false && right === false) {
+            return true
+        }
+        else {
+            return "--ERROR--"
+        }
     }
 }
-
-console.log(logic(true, false, 'and'));
