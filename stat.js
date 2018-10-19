32216decbe2954702a39ac17f68242efebdb7838
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-10-20 05:41:12 +0700

    Edit Mode Method and Change the class into Module Exports.

--- stat.js
+++ stat.js
@@ -1,4 +1,4 @@
-function Stat() {
+module.exports = function() {
     let data = []
 
     this.leng = function() {
@@ -20,25 +20,25 @@ function Stat() {
     }
 
     this.sum = function() {
-        let s = 0
+        let summation = 0
         data.forEach(el => {
-            s += el
+            summation += el
         })
-        return s
+        return summation
     }
 
     this.mean = function() {
-        return this.sum() / data.length
+        return (this.sum() / data.length).toFixed(2)
     }
 
     this.median = function() {
         if(data.length % 2 === 0) {
             medindex = data.length / 2
-            return ((data[medindex - 1] + data[medindex]) / 2)
+            return ((data[medindex - 1] + data[medindex]) / 2).toFixed(2)
         }
         else if(data.length % 2 !== 0) {
             medindex = Math.ceil(data.length / 2)
-            return data[medindex-1]
+            return data[medindex - 1].toFixed(2)
         }
     }
 
@@ -48,8 +48,8 @@ function Stat() {
 
     this.mode = function() {
       let count = []
-      data.forEach(el => {
-        count.push(data.filter(x => x === el).length);
+      data.forEach(element => {
+        count.push(this.frequency(element));
       })
       lenmax = Math.max(...count)
       countmax = count.indexOf(lenmax)
@@ -68,35 +68,6 @@ function Stat() {
         return this.maxi() - this.mini()
     }
 }
-
-let browser = new Stat()
-browser.addData(1)
-browser.addData(2)
-browser.addData(3)
-browser.addData(4)
-browser.addData(4)
-browser.delData(3)
-console.log(browser.disPlay())
-console.log(browser.sum())
-console.log(browser.mean())
-console.log(browser.median())
-console.log(browser.leng())
-console.log(browser.maxi())
-console.log(browser.mini())
-console.log(browser.range())
-console.log(browser.frequency(4))
-console.log(browser.mode())
-
-// data = [1,2,3,3,4,4,5]
-// count = []
-
-// data.forEach((el) => {
-//     count.push(data.filter(x => x === el).length)
-// })
-
-// lenmax = Math.max(...count)
-// countmax = count.indexOf(lenmax);
-// console.log(data[countmax]);
 
 
 
